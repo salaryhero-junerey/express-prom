29bf22e8f20a856a531a8fde6dae372b988b4677
Junerey <junerey@salary-hero.com>
2024-11-14 09:46:31 +0700

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+
+const promBundle = require('express-prom-bundle')
+
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  includeStatusCode: true,
+  includeUp: true,
+  customLabels: { project: 'my-project' },
+  promClient: {
+    collectDefaultMetrics: {}
+  }
+})
+
+app.use(metricsMiddleware)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(3000, () => { console.log('API is running') })
